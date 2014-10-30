655f1a9d01dda136616b28d2ba04cc26651e5cfc
Ellaylone <heamik91@yandex.ru>
2014-10-31 00:33:44 +0600

    работа с файлом, поиск
    добавил тестовый файл, набил временные процедуры для работы с файлом,
    сделал обманку поиска, хз как использовать процедуры из другого файла

--- asoiu1.pas
+++ asoiu1.pas
@@ -1,18 +1,12 @@
 Program AirportHelp1;
-uses App, Objects, Views, Drivers, Menus, flightWindows, flightModel;
+uses App, Objects, Views, Drivers, Menus, flightWindows;
 type
   PInterior = ^TInterior;
   TInterior = object(TView)
     constructor Init(var Bounds: TRect);
     procedure Draw; virtual;
   end;
-type
-        PFlight = ^TFlight;
-	TFlight = record
-		Id, Price: integer;
-		FromPoint, ToPoint, Date, Time: String;
-		Next: PFlight;
-	end;
+
 
 //constructor TAirHelp.Init;
 //var Window: PWindow; R:TRect;
@@ -53,11 +47,58 @@ begin
   Interior := New(PInterior, Init(Bounds));
   Insert(Interior);    { добавляет интерьер к окну }
 end;}
+type
+        PFlight = ^TFlight;
+	TFlight = record
+		Id, Price: integer;
+		FromPoint, ToPoint, Date, Time: String;
+		Next: PFlight;
+	end;
 
 var AirHelp: TAirHelp;
+  data: file of PFlight;
+  test: PFlight;
+          procedure writeFlight();
 begin
-	AirHelp.Init();
-        AirHelp.NewWindow();
-	AirHelp.Run;
-	AirHelp.Done;
+     new(test);
+     test^.Id := 1;
+     test^.Price := 10;
+     test^.Date := '01.01.1990';
+     test^.FromPoint := 'kurgan';
+     test^.ToPoint := 'tomsk';
+     test^.Time := '18:00';
+     test^.Next := nil;
+     Assign(data, 'data');
+     reset(data);
+     write(data, test);
+     CloseFile(data);
+end;
+procedure readFlight();
+begin
+     Assign(data, 'data');
+     reset(data);
+     while not eof(data) do
+           begin
+             Read(data, test);
+           end;
+     writeln(test^.Id, test^.Price, test^.Date);
+     CloseFile(data);
+end;
+procedure findFlight();
+begin
+     // идти по порядку
+     if test^.Date = '01.01.1990' then begin
+        writeln(test^.Date);
+     end;
+end;
+begin
+//	AirHelp.Init();
+  //      AirHelp.NewWindow();
+//	AirHelp.Run;
+//	AirHelp.Done;
+        writeFlight();
+        readFlight();
+        writeln();
+        findFlight();
+        readln();
 end.
